a01364c7e4c7633efb3633980d12e1c537af033a
Koderkup <koderkup@gmail.com>
2023-11-05 18:04:15 +0300

    loading made

--- pages/login/index.js
+++ pages/login/index.js
@@ -1,13 +1,65 @@
 import Head from "next/head";
 import Link from "next/link";
+import { useState, useContext, useEffect } from "react";
+import { DataContext } from "@/store/GlobalState";
+import { postData } from "@/utils/fetchData";
+import Cookie from "js-cookie";
+import { useRouter } from "next/router";
 
 function Login() {
+  const initialState = { email: "", password: "" };
+  const [userData, setUserData] = useState(initialState);
+  const { email, password } = userData;
+
+  const { state, dispatch } = useContext(DataContext);
+  const { auth } = state;
+
+  const router = useRouter();
+
+  const handleChangeInput = (e) => {
+    const { name, value } = e.target;
+    setUserData({ ...userData, [name]: value });
+    dispatch({ type: "NOTIFY", payload: {} });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    dispatch({ type: "NOTIFY", payload: { loading: true } });
+    const res = await postData("auth/login", userData);
+
+    if (res.err)
+      return dispatch({ type: "NOTIFY", payload: { error: res.err } });
+    dispatch({ type: "NOTIFY", payload: { success: res.msg } });
+
+    dispatch({
+      type: "AUTH",
+      payload: {
+        token: res.access_token,
+        user: res.user,
+      },
+    });
+
+    Cookie.set("refreshtoken", res.refresh_token, {
+      path: "api/auth/accessToken",
+      expires: 7,
+    });
+
+    localStorage.setItem("firstLogin", true);
+  };
+
+  useEffect(() => {
+    if (Object.keys(auth).length !== 0) router.push("/");
+  }, [auth]);
   return (
     <div>
       <Head>
         <title>Страница авторизации</title>
       </Head>
-      <form className="mx-auto my-4" style={{maxWidth: '500px'}}>
+      <form
+        className="mx-auto my-4"
+        style={{ maxWidth: "500px" }}
+        onSubmit={handleSubmit}
+      >
         <div className="mb-3">
           <label htmlFor="exampleInputEmail1" className="form-label">
             Email адрес
@@ -16,8 +68,11 @@ function Login() {
             type="email"
             className="form-control"
             id="exampleInputEmail1"
+            name="email"
+            value={email}
+            onChange={handleChangeInput}
             aria-describedby="emailHelp"
-            autoComplete="user-name"
+            autoComplete="user-email"
           />
         </div>
         <div className="mb-3">
@@ -27,8 +82,10 @@ function Login() {
           <input
             type="password"
             className="form-control"
-            id="exampleInputPassword1"
             autoComplete="current-password"
+            name="password"
+            value={password}
+            onChange={handleChangeInput}
           />
         </div>
         <div className="mb-3 form-check">
@@ -44,7 +101,12 @@ function Login() {
         <button type="submit" className="btn btn-primary w-100">
           Вход
         </button>
-        <p className="my-2">Ещё не зарегистрировались? <Link style={{color: 'crimson'}} href='/register'>Регистрация</Link></p>
+        <p className="my-2">
+          Ещё не зарегистрировались?{" "}
+          <Link style={{ color: "crimson" }} href="/register">
+            Регистрация
+          </Link>
+        </p>
       </form>
     </div>
   );
