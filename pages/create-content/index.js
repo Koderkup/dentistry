43f8ee6443b8e62e96d58ddde90f8cde69ca1d32
Koderkup <koderkup@gmail.com>
2024-01-31 13:50:23 +0300

    remove doctorpersondefault

--- pages/create-content/index.js
+++ pages/create-content/index.js
@@ -1,11 +1,9 @@
 import React from "react";
-import { DoctorPersonDefault } from "@/components/doctor/DoctorPerson";
 import { ServiceItemDefault } from "@/components/service/ServiceItem";
 const CreateContent = () => {
   return (
     <>
       <div className="container">
-        <DoctorPersonDefault />
       </div>
         <ServiceItemDefault />
     </>
